8ca2571eaaa99f4127353e16f0a8af082451562e
johansballestar <johansballestar@gmail.com>
2022-03-01 20:58:15 -0600

    Added contracts

---
+++ contracts/Draft.sol
@@ -0,0 +1,169 @@
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "../lib/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import "../interfaces/IProton.sol";
+import "../interfaces/IUniverse.sol";
+import "../interfaces/IChargedState.sol";
+import "../interfaces/IChargedSettings.sol";
+import "../interfaces/IChargedParticles.sol";
+
+import "../lib/BlackholePrevention.sol";
+import "../lib/RelayRecipient.sol";
+
+contract BoAHeroes is IProton, ERC721, Ownable, RelayRecipient, ReentrancyGuard, BlackholePrevention {
+  using SafeMath for uint256;
+  using Address for address payable;
+  using Counters for Counters.Counter;
+
+  Counters.Counter private supply;
+
+  uint256 constant internal PERCENTAGE_SCALE = 1e4;   // 10000  (100%)
+  uint256 constant internal MAX_ROYALTIES = 8e3;      // 8000   (80%)
+
+
+  string public uriPrefix = "QmUsX3TvYuozXR6ihBjDxd4Cq3xrj7hWE3R8P1BrsVAQ94";
+  string public uriSuffix = ".json";
+  string public hiddenMetadataUri;
+  
+  uint256 public cost = 0.1 ether; //Should be .07 eth
+  uint256 public maxSupply = 10000; 
+  uint256 public maxMintAmountPerTx = 50; //Should be 10
+
+  bool public paused = true;
+  bool public revealed = false;
+
+  constructor() ERC721("BoAHero", "BoA") {
+    setHiddenMetadataUri("ipfs://QmUsX3TvYuozXR6ihBjDxd4Cq3xrj7hWE3R8P1BrsVAQ94/hidden.json");
+  }
+
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+  }
+
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(!paused, "The contract is paused!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+    _mintLoop(msg.sender, _mintAmount);
+  }
+  
+  function mintForAddress(uint256 _mintAmount, address _receiver, string memory walletMangerId, address assetToken, unit256 assetAmount) public mintCompliance(_mintAmount) onlyOwner {
+    _mintLoop(_receiver, _mintAmount, walletMangerId, assetToken, assetAmount );
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+    uint256 currentTokenId = 1;
+    uint256 ownedTokenIndex = 0;
+
+    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
+      address currentTokenOwner = ownerOf(currentTokenId);
+
+      if (currentTokenOwner == _owner) {
+        ownedTokenIds[ownedTokenIndex] = currentTokenId;
+
+        ownedTokenIndex++;
+      }
+
+      currentTokenId++;
+    }
+
+    return ownedTokenIds;
+  }
+
+  function tokenURI(uint256 _tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(_tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+        : "";
+  }
+
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
+  }
+
+  function setCost(uint256 _cost) public onlyOwner {
+    cost = _cost;
+  }
+
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+  }
+
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
+
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
+
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
+
+  function setPaused(bool _state) public onlyOwner {
+    paused = _state;
+  }
+
+  function withdraw() public onlyOwner {
+    // This will pay CryptoKatt 5% of the initial sale.
+    // =============================================================================
+    (bool hs, ) = payable(0x781E0bc357252372A9444cD32A6F8cB7f6540C90).call{value: address(this).balance * 5 / 100}("");
+    require(hs);
+    // =============================================================================
+
+    // This will transfer the remaining contract balance to the owner.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
+
+  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      supply.increment();
+      _safeMint(_receiver, supply.current());
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
+  }
+}
+
